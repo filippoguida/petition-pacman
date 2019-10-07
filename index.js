c11c3e7c56440104521125eabfd51e8ea5797046
Filippo Guida <filippo.guida@icloud.com>
2019-10-07 15:11:51 +0200

    code refactoring and routes connections

--- index.js
+++ index.js
@@ -1,41 +1,56 @@
 const db = require("./db");
-const bp = require("body-parser");
-const hb = require("express-handlebars");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const handlebars = require("express-handlebars");
 const express = require("express");
 const app = express();
 
-app.engine("handlebars", hb());
+app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
-app.use(bp.urlencoded({ extended: false }));
-app.use(bp.json());
+
+app.use(
+    cookieSession({
+        keys: [`coriander-petition-filippoguida-secret`],
+        maxAge: 1000 * 60 * 60 * 24 * 14 //2 weeks
+    })
+);
 
 app.get("/petition", (req, res) => {
-    res.render("index", {
-        layout: "main"
-    });
+    let id = req.session.signatureId;
+    if (!id) res.render("petition");
+    else res.redirect("/thanks");
 });
 
 app.post("/petition", (req, res) => {
-    res.send(db.addSignature(req.body).catch(e => console.log(e)));
-});
-
-app.get("/thanks", (req, res) => {
-    res.render("thanks", {
-        layout: "main"
+    db.addSignature(req.body, {
+        success: id => {
+            req.session.signatureId = id;
+            res.redirect("/thanks");
+        },
+        error: () => res.render("petition", { error: true })
     });
 });
 
+app.get("/thanks", (req, res) => {
+    let id = req.session.signatureId;
+    if (!id) res.redirect("/petition");
+    else
+        db.getSignature(id, {
+            success: signature => res.render("thanks", { signature })
+        });
+});
+
 app.get("/signers", (req, res) => {
-    db.getSignatures()
-        .then(signersTable => {
-            res.render("signers", {
-                layout: "main",
-                signers: signersTable.rows
-            });
-        })
-        .catch(e => console.log(e));
+    let id = req.session.signatureId;
+    if (!id) res.redirect("/petition");
+    else
+        db.getSigners({
+            success: signers => res.render("signers", { signers }),
+            error: () => res.sendStatus(500)
+        });
 });
 
 app.listen(8080, () => {
