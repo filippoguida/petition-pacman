cd8fa475a0f9505ccad51b884452ca60b5ce4ce2
Filippo Guida <filippo.guida@icloud.com>
2019-10-07 07:32:25 +0200

    Added signature form with canvas signature

---
+++ public/js/signature.js
@@ -0,0 +1,20 @@
+const sigDiv = $("#signature-container")[0];
+new window.p5(p5 => {
+    p5.setup = function() {
+        p5.createCanvas(sigDiv.clientWidth, sigDiv.clientHeight);
+        p5.strokeWeight(3);
+    };
+
+    p5.windowResized = () => {
+        p5.resizeCanvas(sigDiv.clientWidth, sigDiv.clientHeight);
+        p5.redraw();
+    };
+
+    p5.mouseDragged = () => {
+        p5.line(p5.pmouseX, p5.pmouseY, p5.mouseX, p5.mouseY);
+    };
+}, sigDiv);
+
+$(document).click(() => {
+    console.log($(".p5Canvas")[0].toDataURL());
+});
