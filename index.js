a9e84dd6202882ec9573a20b211e8cabf7397cad
Filippo Guida <filippo.guida@icloud.com>
2019-10-07 15:17:58 +0200

    protecting against CSRF

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const db = require("./db");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
+const csurf = require("csurf");
 const handlebars = require("express-handlebars");
 const express = require("express");
 const app = express();
@@ -17,6 +18,12 @@ app.use(
         maxAge: 1000 * 60 * 60 * 24 * 14 //2 weeks
     })
 );
+
+app.use(csurf());
+app.use((req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
 
 app.get("/petition", (req, res) => {
     let id = req.session.signatureId;
