a9abf8eeb1f322ccc4c1f21895e8fd8d6e40eeef
Filippo Guida <filippo.guida@icloud.com>
2019-10-07 15:12:23 +0200

    code refactoring, cookies and routes connections

--- db.js
+++ db.js
@@ -1,15 +1,28 @@
 const spicedPg = require("spiced-pg");
 const db = spicedPg(`postgres:postgres:postgres@localhost:5432/petition`);
 
-module.exports.addSignature = newEntry => {
-    let { firstName, lastName, email, signature } = newEntry;
-    return db.query(
-        `INSERT INTO signatures (first_name, last_name, email, signature)
-        VALUES ($1, $2, $3, $4)`,
-        [firstName, lastName, email, signature]
-    );
+module.exports.addSignature = (newEntry, callbacks) => {
+    let { first_name, last_name, email, signature } = newEntry;
+    return db
+        .query(
+            `INSERT INTO signatures (first_name, last_name, email, signature)
+        VALUES ($1, $2, $3, $4) RETURNING id`,
+            [first_name, last_name, email, signature]
+        )
+        .then(sqlTab => callbacks.success(sqlTab.rows[0].id))
+        .catch(err => callbacks.error(err));
 };
 
-module.exports.getSignatures = () => {
-    return db.query(`SELECT first_name, last_name FROM signatures`);
+module.exports.getSigners = callbacks => {
+    return db
+        .query(`SELECT first_name, last_name FROM signatures`)
+        .then(sqlTab => callbacks.success(sqlTab.rows))
+        .catch(err => callbacks.error(err));
+};
+
+module.exports.getSignature = (id, callbacks) => {
+    return db
+        .query(`SELECT signature FROM signatures WHERE id = $1`, [id])
+        .then(sqlTab => callbacks.success(sqlTab.rows[0].signature))
+        .catch(err => callbacks.error(err));
 };
--- public/js/signature.js
+++ public/js/signature.js
@@ -12,19 +12,6 @@ new window.p5(p5 => {
 
     p5.mouseDragged = () => {
         p5.line(p5.pmouseX, p5.pmouseY, p5.mouseX, p5.mouseY);
+        $('input[name="signature"]').val($(".p5Canvas")[0].toDataURL());
     };
 }, sigDiv);
-
-$("#signature-form").submit(e => {
-    e.preventDefault();
-    $.ajax({
-        method: "POST",
-        url: "/petition",
-        data: {
-            firstName: $('input[name="fname"]').val(),
-            lastName: $('input[name="lname"]').val(),
-            email: $('input[name="email"]').val(),
-            signature: $(".p5Canvas")[0].toDataURL("png")
-        }
-    });
-});
