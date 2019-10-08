8e993b037aa0cb6ed2de328aa84448a5fcc4132b
Filippo Guida <filippo.guida@icloud.com>
2019-10-08 17:36:35 +0200

    the DB still uses indipendent table and two ID cookies

--- petitionInit.sql
+++ petitionInit.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS signatures;
 CREATE TABLE signatures (
-    userId SERIAL primary key,
+    id SERIAL primary key,
+    first_name  VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     signature TEXT NOT NULL
 );
 
